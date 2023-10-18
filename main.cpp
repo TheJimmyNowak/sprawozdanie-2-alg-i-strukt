8674474252a038e23c6d78bd04ab3cad73de526f
Mikołaj Data <miki3867@gmail.com>
2023-10-18 18:59:08 +0200

    4.c

--- main.cpp
+++ main.cpp
@@ -6,27 +6,42 @@ using namespace std;
 
 int main() {
   int Z[N];
-  int i, maxZ, p = -1;
+  int i, maxZ, minZ, p = -1;
 
   // Define array
   srand(time(NULL));
   for (i = 0; i < N; i++)
-    Z[i] = rand() % 1000;
+    Z[i] = rand() % 10;
 
   maxZ = 0;
+  minZ = 1000;
   for (i = 1; i < N; i++) {
-    if (Z[i] > maxZ) {
+    if (Z[i] > maxZ)
       maxZ = Z[i];
-    }
-  }
 
-  int frequency = 0;
+    if (Z[i] < minZ)
+      minZ = Z[i];
+  }
+  cout << "min" << minZ << endl;
+  cout << "max" << maxZ << endl;
+
+  int frequencyMin = 0;
+  int frequencyMax = 0;
   for (i = 0; i < N; i++) {
     cout << Z[i] << endl;
+    if (Z[i] == minZ)
+      frequencyMin++;
+
     if (Z[i] == maxZ)
-      frequency++;
+      frequencyMax++;
   }
-  cout << "Max= " << maxZ << endl;
-  cout << "Częstość= " << frequency << endl;
+
+  if (frequencyMax < frequencyMin)
+    cout << "Min występuję częściej (" << frequencyMin << ")" << endl;
+  else if (frequencyMax > frequencyMin)
+    cout << "Max występuję częściej (" << frequencyMax << ")" << endl;
+  else
+    cout << "Min i max wystąpiło tyle samo razy (" << frequencyMin << ")"
+         << endl;
   return 0;
 }
