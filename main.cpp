000280dccfe86877f907ae66b254c2569a612a68
Mikołaj Data <miki3867@gmail.com>
2023-10-18 18:27:16 +0200

    2.c

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main() {
   cin >> key;
   D[n] = key;
   pos = -1;
-  for (int i = 0; i < n / 2; i++) {
+  for (int i = n / 2; i < n; i++) {
     if (D[i] == key) {
       cout << "Pozycja " << i + 1 << endl;
       break;
